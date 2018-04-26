1aae60971b2d7b6cf86ae4a8d51f2ac8a900280b
pedroalopes <lopes.pedroa@gmail.com>
2018-04-26 17:58:43 +0100

    Main vehicle structure done. One wheel incomplete

--- src/MyVehicle.js
+++ src/MyVehicle.js
@@ -12,14 +12,29 @@
         this.lowerBodyWork = new MyUnitCubeQuad(scene);
         this.upperBodyWork = new MyUnitCubeQuad(scene);
 
+        this.wheel = new MyCylinder(scene, 20, 1);
+
   	};
 
     display()
     {
-            //this.scene.pushMatrix();
-            //this.lowerBodyWork.scale();
+            this.scene.pushMatrix();
+        this.scene.scale(1.80, 0.95, 4.30);
         this.lowerBodyWork.display();
+            this.scene.popMatrix();
+
+            this.scene.pushMatrix();
+        this.scene.translate(0, 0.95, 0);
+        this.scene.scale(1.80, 0.95, 4.00);
         this.upperBodyWork.display();
+            this.scene.popMatrix();
+
+            this.scene.pushMatrix();
+        this.scene.translate(0.4, -0.4, 1.5);
+        this.scene.rotate(Math.PI / 2, 0, 1, 0);
+        this.scene.scale(0.3,0.3,1);
+        this.wheel.display();
+            this.scene.popMatrix();
 
     };
 
