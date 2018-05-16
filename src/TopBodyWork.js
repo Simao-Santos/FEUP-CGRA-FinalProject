0a55c28991dcddd622b1073f123cadde0a9be562
PedroLopes <lopes.pedroa@gmail.com>
2018-05-16 23:32:27 +0100

    Last commit problem solved

--- src/TopBodyWork.js
+++ src/TopBodyWork.js
@@ -4,33 +4,19 @@
  */
 class TopBodyWork extends CGFobject
 {
-	constructor(scene, width, height, frontDiff, backDiff)
+	constructor(scene)
 	{
 		super(scene);
 
-        this.width = width;
-        this.height = height;
-        this.frontDiff = frontDiff;
-        this.backDiff = backDiff;
-
-        this.frontPrism = new TriangularPrism(scene, 1);
-        //this.backPrism = new TriangularPrism(scene, 10);
-        this.midBody = new QuadrangularPrism(scene, 4, 1);
+        this.topBody = new MyTrapezePrism(scene, 3.8, 0.1, 0.3, 0.1, 10);
 
 	};
 
     display() {
 
-            this.scene.pushMatrix();
-        this.scene.rotate(Math.PI / 4, 0, 0, 1);
-        this.midBody.display();
-            this.scene.popMatrix();
 
-            this.scene.pushMatrix();
-        this.scene.translate(0, 0, 1);
-        this.scene.rotate( - Math.PI / 2, 0, 1, 0);
-        this.frontPrism.display();
-            this.scene.popMatrix();
+		this.scene.scale(1, 0.9, 1.9);
+        this.topBody.display();
 
     };
 };
---
+++ src/Wheel.js
@@ -0,0 +1,33 @@
+/**
+ * Wheel
+ * @constructor
+ */
+
+  class Wheel extends CGFobject
+  {
+  	constructor(scene)
+  	{
+        super(scene);
+
+        this.tire = new MyCylinder(scene, 20, 1);
+        this.front = new MyCircle(scene, 20);
+
+  	};
+
+    display()
+    {
+            this.scene.pushMatrix();
+        this.scene.scale(0.5,0.5,0.2);
+        this.tire.display();
+            this.scene.popMatrix();
+
+            this.scene.pushMatrix();
+        this.scene.scale(0.5, 0.5, 1);
+        this.scene.translate(0, 0, 0.2);
+        this.front.display();
+            this.scene.popMatrix();
+
+    };
+
+
+ }
